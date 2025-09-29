abd2ee6ea43682f1009bbc8d8431299dd0043235
IS16 <sarkisov775@yandex.ru>
2025-09-29 19:04:07 +0300

    1 итерация

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -6,17 +6,39 @@ describe('PostsService', () => {
     text: 'Mocked post',
   };
 
+  let createdPost: Post;
+  let createdPostId: string;
+  let foundPost: Post | undefined;
+
   beforeEach(async () => {
     postsService = new PostsService();
 
     postsService.create({ text: 'Some pre-existing post' });
+
+    createdPost = postsService.create(post);
+    createdPostId = createdPost.id;
+
+    foundPost = postsService.find(createdPostId);
   });
 
   it('should add a new post', () => {
-    // реализуйте тест-кейс
+    expect(createdPost).toBeDefined();
+
+    expect(createdPost?.id).toBeDefined();
+    expect(createdPost?.text).toBeDefined();
+    expect(createdPost?.date).toBeDefined();
+
+    expect(foundPost).toBeDefined();
+
   });
 
   it('should find a post', () => {
-    // реализуйте тест-кейс
+    expect(foundPost).toBeDefined();
+
+    expect(foundPost?.id).toBeDefined();
+    expect(foundPost?.text).toBeDefined();
+
+    expect(foundPost?.id).toEqual(createdPostId);
+    expect(foundPost?.text).toEqual(post.text);
   });
 });
